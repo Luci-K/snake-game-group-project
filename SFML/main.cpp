32c395393f11e8f4086ebf886527b1cbe76fa9ea
Lucina Kerrigan <Kerrigan-C9@ulster.ac.uk>
2021-11-11 14:44:10 +0000

    changes

--- SFML/main.cpp
+++ SFML/main.cpp
@@ -11,12 +11,11 @@
 int main()
 {
 #pragma region Window Creation
-    sf::RenderWindow window(sf::VideoMode(1200, 800), "Plot Points!", sf::Style::Default, sf::ContextSettings(24));   // render a Window
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "Snake Gameing", sf::Style::Default, sf::ContextSettings(24));   // render a Window
     window.setVerticalSyncEnabled(true);
     sf::Vector2u winSize = window.getSize();
 #pragma endregion
-    graphPoints graphPts;                                               // create an instance of my point plotting class
-    graphPts.loadPoints("HeartRate.csv", window);
+    
     while (window.isOpen())                                             // This is the Windows application loop - infinite loop until closed
     {
 #pragma region Check for Exit
@@ -29,21 +28,17 @@ int main()
 
 #pragma endregion
         
-        // Set up x and y axis lines - this could be modified to look better. Further lines can be drawn in the same way. 
-        // Lines can also be draw more efficiently with vertices. https://en.sfml-dev.org/forums/index.php?topic=11121.0
-        // A function for this code would be better.
-        sf::RectangleShape horizLine; sf::RectangleShape vertLine;
-        horizLine.setFillColor(sf::Color(0,0,0)); horizLine.setSize(sf::Vector2f(1000, 2)); horizLine.setPosition(50, winSize.y - 50); horizLine.setRotation(0);
-        vertLine.setFillColor(sf::Color(0,0,0)); vertLine.setSize(sf::Vector2f(700, 2));  vertLine.setPosition(50, winSize.y - 50); vertLine.setRotation(-90); // anticlockwise
+#pragma region Create Window Border
+        sf::RectangleShape horizontalborder(sf::Vector2f(winSize.x, 10)); horizontalborder.setFillColor(sf::Color(176, 11, 105)); horizontalborder.setPosition(0, winSize.y-800);
+        
+#pragma endregion 
 
-        window.clear(sf::Color(168, 168, 168));                         // Clear graphics buffer
-
-        window.draw(horizLine);                                         // Draw x and y axis lines 
-        window.draw(vertLine);
-        graphPts.drawPoints(window);                                    // Call draw function in my class
-
+        window.clear();                                                 // Clear graphics buffer
+        window.draw(horizontalborder);
         window.display();                                               // Display the graphics from the buffer to the display
+        
     }
+
 
     return 0;
 }
